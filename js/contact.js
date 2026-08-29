8bb18ee54fa156740a66a46286b29a8b7a4a0662
Vejen-control <111349061+Vejen-control@users.noreply.github.com>
2026-08-29 12:09:40 +0100

    Add files via upload

---
+++ js/contact.js
@@ -0,0 +1,97 @@
+const SCRIPT_URL =
+"https://script.google.com/macros/s/AKfycby6qi0zfvIYA1zvXZLGIch6v3-Dkxcuxar7WbTlBhrMqkVYT5afdg7HmKdQXHexRyaO/exec";
+
+const form = document.getElementById("contact-form");
+const status = document.getElementById("status");
+
+form.addEventListener("submit", async function(e){
+
+    e.preventDefault();
+
+    status.innerHTML =
+    "<p style='color:#ffd700'>Sending your enquiry...</p>";
+
+    const data = {
+
+        name: document.getElementById("name").value,
+
+        company: document.getElementById("company").value,
+
+        email: document.getElementById("email").value,
+
+        subject: document.getElementById("subject").value,
+
+        message: document.getElementById("message").value
+
+    };
+
+    try{
+
+        const response = await fetch(SCRIPT_URL, {
+            method:"POST",
+            headers:{
+        "Content-Type":"text/plain;charset=utf-8"
+        },
+        body:JSON.stringify(data)
+    });
+
+        const result = await response.json();
+
+        if(result.success){
+
+            status.innerHTML =
+            `
+            <div style="
+                margin-top:25px;
+                padding:25px;
+                border-radius:12px;
+                background:rgba(0,0,0,.55);
+                border:1px solid #c8a23d;
+                color:white;
+                text-align:center;
+            ">
+
+                <h2 style="color:#FFD700;">
+                    Thank you.
+                </h2>
+
+                <p>Your enquiry has been received successfully.</p>
+
+                <p>
+                    Reference Number
+                </p>
+
+                <h3 style="color:#FFD700">
+                    ${result.reference}
+                </h3>
+
+                <p>
+                    We normally reply within 24 hours.
+                </p>
+
+            </div>
+            `;
+
+            form.reset();
+
+        }
+
+        else{
+
+            status.innerHTML =
+            "<p style='color:red'>Error: " + result.error + "</p>";
+
+        }
+
+    }
+
+    catch(error){
+
+        console.log(error);
+
+        status.innerHTML =
+        "<p style='color:red'>Connection error.</p>";
+
+    }
+
+});
